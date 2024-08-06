5cfad654d49d92615b7ac658f03450ae8e20fed1
Eli <elicodesforfun@gmail.com>
2024-08-06 12:56:22 +0800

    chore: swap expected and actual

--- evm/evm_test.go
+++ evm/evm_test.go
@@ -18,14 +18,12 @@ func TestEvmTranfers(t *testing.T) {
 		big.NewInt(20359098),
 	)
 
-	transfer := Transfer{
+	assert.Equal(t, Transfer{
 		from:         "0xA5bA9D68890D0BA1C7d5c6D1AE9B2836a5c4F4f1",
 		to:           "0xA5bA9D68890D0BA1C7d5c6D1AE9B2836a5c4F4f1",
 		amount:       "1",
 		txid:         "0xb831414248618c196919908bfdd05106ee7b6ab57ed8bac986374d8de7191902",
 		timestamp:    "0x884dd19c0e966eaf5b3e37e4df55a1995a243aa352e70794f0a249e7828fc274",
 		transferType: "SEND",
-	}
-
-	assert.Equal(t, transfers[0], transfer)
+	}, transfers[0])
 }
