cd7659865829d09c611685f4d5f0d4585144931c
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2024-08-07 11:06:58 +0800

    feat: Add Tron ChainNames

--- chains/chains.go
+++ chains/chains.go
@@ -6,11 +6,13 @@ const (
 	Ethereum        ChainName = "ethereum"
 	EthereumSepolia ChainName = "ethereumSepolia"
 	Polygon         ChainName = "polygon"
+	Tron            ChainName = "tron"
+	TronShasta      ChainName = "tronShasta"
 )
 
 func IsValidChain(chain ChainName) bool {
 	switch chain {
-	case Ethereum, EthereumSepolia, Polygon:
+	case Ethereum, EthereumSepolia, Polygon, Tron, TronShasta:
 		return true
 	default:
 		return false
--- chains/chains_test.go
+++ chains/chains_test.go
@@ -9,4 +9,6 @@ func TestChains(t *testing.T) {
 	assert.Equal(t, Ethereum, ChainName("ethereum"))
 	assert.Equal(t, EthereumSepolia, ChainName("ethereumSepolia"))
 	assert.Equal(t, Polygon, ChainName("polygon"))
+	assert.Equal(t, Tron, ChainName("tron"))
+	assert.Equal(t, TronShasta, ChainName("tronShasta"))
 }
